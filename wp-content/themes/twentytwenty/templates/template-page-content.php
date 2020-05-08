966f6f75cc173d04a2a2928796855f36d9d6f68b
vienvu <vien@vienvu.com>
2020-05-08 09:30:43 +0700

    new

---
+++ wp-content/themes/twentytwenty/templates/template-page-content.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Content page
+ * Template Post Type:  page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+	<div class="section-inner">
+	<?php
+
+	if ( have_posts() ) {
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content-page', get_post_type() );
+		}
+	}
+
+	?>
+	</div>
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php get_footer(); ?>
