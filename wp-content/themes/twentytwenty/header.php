2439d400e7f84188e21c544a2264fe7e2e5e4275
vienvu <vien@vienvu.com>
2020-05-11 10:33:09 +0700

    new

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -29,7 +29,7 @@ $prefix = $vi ? '/vi' : '';
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 		<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 		<link rel="stylesheet" id="twentytwenty-style-css" href="/wp-content/themes/twentytwenty/custom.css?ver=1.1" type="text/css" media="all">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700&display=swap&subset=vietnamese" rel="stylesheet">
+		<!-- <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700&display=swap&subset=vietnamese" rel="stylesheet"> -->
 
         <script
   src="https://code.jquery.com/jquery-3.4.1.js"
